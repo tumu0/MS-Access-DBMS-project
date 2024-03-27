f911a8aec2f967f5bc9cf70512e33155d6507ceb
tumu0 <165114819+tumu0@users.noreply.github.com>
2024-03-27 06:39:23 +0300

    Add files via upload

---
+++ WindowsFormsApp9/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace WindowsFormsApp9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 13F);
+            this.button1.Location = new System.Drawing.Point(557, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "التالي";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Tahoma", 13F);
+            this.button2.Location = new System.Drawing.Point(418, 274);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "السابق";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Tahoma", 13F);
+            this.button3.Location = new System.Drawing.Point(236, 274);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 43);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "إضافة";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Tahoma", 13F);
+            this.button4.Location = new System.Drawing.Point(97, 274);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 43);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "حذف";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Tahoma", 13F);
+            this.textBox1.Location = new System.Drawing.Point(314, 57);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(258, 36);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Tahoma", 13F);
+            this.textBox2.Location = new System.Drawing.Point(314, 116);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(258, 36);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Tahoma", 13F);
+            this.textBox3.Location = new System.Drawing.Point(314, 174);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(258, 36);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Tahoma", 13F);
+            this.label1.Location = new System.Drawing.Point(596, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 36);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "الإسم";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Tahoma", 13F);
+            this.label2.Location = new System.Drawing.Point(596, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 36);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "الرقم";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Tahoma", 13F);
+            this.label4.Location = new System.Drawing.Point(596, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 36);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "الإيميل";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(621, 323);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(69, 25);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "refresh";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Location = new System.Drawing.Point(97, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(156, 153);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 372);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ WindowsFormsApp9/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        private byte[] imageBytes;
+        private OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Admin\Desktop\db\testdb.accdb");
+        private DataTable dataTable;
+        private int currentRecordIndex = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+
+                string query = "SELECT * FROM data";
+                OleDbDataAdapter adapter = new OleDbDataAdapter(query, conn);
+                dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                MessageBox.Show("تم الاتصال بنجاح");
+
+                
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("حدث خطأ: " + ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (IsDataAlreadyExists())
+                {
+                    MessageBox.Show("البيانات موجودة بالفعل.");
+                    return;
+                }
+
+                if (pictureBox1.Image != null)
+                {
+                    string query = "INSERT INTO data (the_name, [number], email, picture) VALUES (@name, @num, @email, @pic)";
+                    using (OleDbCommand cmd = new OleDbCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@name", textBox1.Text);
+                        cmd.Parameters.AddWithValue("@num", textBox2.Text);
+                        cmd.Parameters.AddWithValue("@email", textBox3.Text);
+                        cmd.Parameters.AddWithValue("@pic", imageBytes);
+
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    MessageBox.Show("تم الحفظ بنجاح");
+
+                    
+                    LoadData();
+
+                    ClearTextBoxes();
+                    pictureBox1.Image = null;
+                }
+                else
+                {
+                    MessageBox.Show("الرجاء اختيار صورة قبل الحفظ.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("حدث خطأ أثناء حفظ البيانات: " + ex.Message);
+            }
+        }
+
+        private bool IsDataAlreadyExists()
+        {
+            string theName = textBox1.Text;
+            string num = textBox2.Text;
+
+            string query = "SELECT COUNT(*) FROM data WHERE the_name = @name AND [number] = @num";
+            using (OleDbCommand command = new OleDbCommand(query, conn))
+            {
+                command.Parameters.AddWithValue("@name", theName);
+                command.Parameters.AddWithValue("@num", num);
+
+                int count = (int)command.ExecuteScalar();
+                return count > 0;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.jpg, *.png, *.bmp)|*.jpg;*.png;*.bmp";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(openFileDialog.FileName);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    pictureBox1.Image.Save(ms, pictureBox1.Image.RawFormat);
+                    imageBytes = ms.ToArray();
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataTable.Rows.Count == 0)
+                {
+                    MessageBox.Show("لا توجد بيانات لحذفها.");
+                    return;
+                }
+
+                string query = "DELETE FROM data WHERE id = @id";
+                using (OleDbCommand cmd = new OleDbCommand(query, conn))
+                {
+                    
+                    int currentRecordId = (int)dataTable.Rows[currentRecordIndex]["id"];
+                    cmd.Parameters.AddWithValue("@id", currentRecordId);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("تم حذف السجل بنجاح");
+
+                        
+                        LoadData();
+
+                        ClearTextBoxes();
+                        pictureBox1.Image = null;
+                    }
+                    else
+                    {
+                        MessageBox.Show("لم يتم العثور على سجل يطابق البيانات المدخلة.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("حدث خطأ أثناء حذف البيانات: " + ex.Message);
+            }
+        }
+
+        private void ClearTextBoxes()
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (currentRecordIndex < dataTable.Rows.Count - 1)
+            {
+                currentRecordIndex++;
+                DisplayCurrentRecord();
+            }
+            else
+            {
+                MessageBox.Show("هذا هو آخر سجل.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (currentRecordIndex > 0)
+            {
+                currentRecordIndex--;
+                DisplayCurrentRecord();
+            }
+            else
+            {
+                MessageBox.Show("هذا هو أول سجل.");
+            }
+        }
+
+        private void DisplayCurrentRecord()
+        {
+            DataRow row = dataTable.Rows[currentRecordIndex];
+            textBox1.Text = row["the_name"].ToString();
+            textBox2.Text = row["number"].ToString();
+            textBox3.Text = row["email"].ToString();
+
+            byte[] imageBytes = (byte[])row["picture"];
+            using (MemoryStream ms = new MemoryStream(imageBytes))
+            {
+                pictureBox1.Image = Image.FromStream(ms);
+            }
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                string query = "SELECT * FROM data";
+                OleDbDataAdapter adapter = new OleDbDataAdapter(query, conn);
+                dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                currentRecordIndex = 0;
+                DisplayCurrentRecord();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("حدث خطأ أثناء تحميل البيانات: " + ex.Message);
+            }
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            ClearTextBoxes();
+            pictureBox1.Image = null;
+        }
+    }
+}
